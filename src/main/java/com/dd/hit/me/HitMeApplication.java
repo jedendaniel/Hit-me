38df573760d80356d46dd7276e236537fbc15ef4
Daniel Kozlik <jedendaniel@gmail.com>
2020-07-26 17:43:10 +0200

    initialize project

---
+++ src/main/java/com/dd/hit/me/HitMeApplication.java
@@ -0,0 +1,13 @@
+package com.dd.hit.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dd/hit/me/HitMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.hit.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
